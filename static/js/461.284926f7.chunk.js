5360e7044b7d5e3773def9ad49b93a6dde1f5179
vlad-Kriajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-23 21:13:14 +0000

    Deploying to gh-pages from @ vlad-Kriajenkov/test-icap-group-gmbh@5ca4037eacdd428d5c1c9c41cfbeb59b95768ffc 🚀

---
+++ static/js/461.284926f7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktest_icap_group_gmbh=self.webpackChunktest_icap_group_gmbh||[]).push([[461],{7461:function(e,t,u){u.r(t);var n=u(2791),c=u(1175),r=u(7691),s=u(184);t.default=function(){var e=(0,r.T)();return(0,n.useEffect)((function(){e((0,c.$)())}),[e]),(0,s.jsx)(s.Fragment,{children:"Home d"})}}}]);
+//# sourceMappingURL=461.284926f7.chunk.js.map
