673ffb02d7af7d994dd893c64a2e6e7acd578d4d
VladKrajenkov <57058826+vlad-Kriajenkov@users.noreply.github.com>
2023-10-25 21:18:22 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/test-icap-group-gmbh">
+    <BrowserRouter basename="/">
       <React.StrictMode>
         <App />
       </React.StrictMode>
